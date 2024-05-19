e9304f8d13eecdf8e08f0ba0a26f292da6e1ec02
niteesh-07 <nitesh07official@gmail.com>
2024-05-19 01:03:00 +0000

    chai with javascript repo

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const account_Id=144553
+let account_Email="nitesh07official@gmail.com"
+var account_Password="12345"
+account_City="Jaipur"
+
+//account_Id=2
+account_Email="nm@nm.com"
+account_Password="21212121"
+account_City="Gaur"
+
+console.log(account_Id)
+
+console.table([account_Id,account_Email,account_Password,account_City])
