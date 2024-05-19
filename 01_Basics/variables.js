59c2a7b57849b3b14fd9e1037461d77188fd6ade
niteesh-07 <nitesh07official@gmail.com>
2024-05-19 01:09:18 +0000

    chai with javascript by youtube

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -2,12 +2,16 @@ const account_Id=144553
 let account_Email="nitesh07official@gmail.com"
 var account_Password="12345"
 account_City="Jaipur"
-
+let account_State;
 //account_Id=2
 account_Email="nm@nm.com"
 account_Password="21212121"
 account_City="Gaur"
 
-console.log(account_Id)
+console.log(account_Id);
 
-console.table([account_Id,account_Email,account_Password,account_City])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
