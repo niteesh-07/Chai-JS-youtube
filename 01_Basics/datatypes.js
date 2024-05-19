9b0eb2eca6b4d71ca1f1d5c9cd1009d8ad4c06f4
niteesh-07 <nitesh07official@gmail.com>
2024-05-19 16:30:01 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)  //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+
+    console.log("Nitesh")
+
+let name="Nitesh"
+let age=19
+let isLOggedIn=false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean=> true/flse
+//null=> standalone value
+//undefined=>
+//symbol => unique
+
+//object
+
+console.log(typeof "Nitesh")
