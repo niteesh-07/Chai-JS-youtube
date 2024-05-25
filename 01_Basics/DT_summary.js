77bda1c85bb47b02fce366a7073e3d4df5ff2c9a
niteesh-07 <nitesh07official@gmail.com>
2024-05-25 15:09:52 +0000

    datatypes summary

---
+++ 01_Basics/DT_summary.js
@@ -0,0 +1,35 @@
+// #Primitive
+
+// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 2313216763n
+
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions  
+
+const heros =["shaktiman", "naagraj", "doga"]
+letmyObj = {
+    name:"Nitesh"
+    age: 22;
+}
+
+const myFunction=(){
+    console.log("hello world");
+}
+
+console.log(typeof scoreValue);
